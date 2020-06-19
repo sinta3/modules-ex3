edd8e41c3af78497401eb233e4d50bdd09112ef2
sinta3 <sinta.tri38@gmail.com>
2020-06-19 16:33:33 +0700

    revisi function

--- function.js
+++ function.js
@@ -1,15 +1,15 @@
 function change(text) {
-    let regex1 = /[a-z]{1,}/;
+    let regex1 = /[A-Z]{1,}/;
     let div = document.getElementById("content");
 
     if (regex1.test(text) == true) {
         let p = document.createElement("p");
         div.appendChild(p);
-        p.innerHTML = `${text.toUpperCase()}`;
+        p.innerHTML = `${text.toLowerCase()}`;
     } else {
         let p = document.createElement("p");
         div.appendChild(p);
-        p.innerHTML = `${text.toLowerCase()}`;
+        p.innerHTML = `${text.toUpperCase()}`;
     }
 }
 export { change };
