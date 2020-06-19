834d84f45630820d7dae8532641ba402050063e1
sinta3 <sinta.tri38@gmail.com>
2020-06-19 16:15:46 +0700

    add a change

--- function.js
+++ function.js
@@ -1,19 +1,15 @@
 function change(text) {
     let regex1 = /[a-z]{1,}/;
-    let regex2 = /[A-Z]{1,}/;
     let div = document.getElementById("content");
 
     if (regex1.test(text) == true) {
         let p = document.createElement("p");
         div.appendChild(p);
         p.innerHTML = `${text.toUpperCase()}`;
-    } else if (regex2.test(text) == true) {
+    } else {
         let p = document.createElement("p");
         div.appendChild(p);
         p.innerHTML = `${text.toLowerCase()}`;
-    } else {
-        console.log("false");
     }
 }
-
 export { change };
